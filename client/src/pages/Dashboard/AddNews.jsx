f32fab540d5f628f56d482bf50f0c9b563ca807b
ishtiyak98 <rifatishtiyak@gmail.com>
2023-05-07 18:43:59 +0600

    multi-select input

--- client/src/pages/Dashboard/AddNews.jsx
+++ client/src/pages/Dashboard/AddNews.jsx
@@ -1,6 +1,8 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import "../../styles/Dashboard/AddNews.scss";
-import { BsImage } from "react-icons/bs";
+import { BsCheckCircle, BsCheckCircleFill, BsImage } from "react-icons/bs";
+import { IoIosArrowDropdown } from "react-icons/io";
+import JoditEditor from "jodit-react";
 
 const AddNews = () => {
   const [formData, setFormData] = useState({
@@ -11,6 +13,13 @@ const AddNews = () => {
   const [selectedImages, setSelectedImages] = useState([]);
   const [imagesError, setImagesError] = useState("");
 
+  const [dropdownDisplay, setDropDownDisplay] = useState(false);
+  const [selectedCategories, setSelectedCategories] = useState([]);
+
+  const editor = useRef(null);
+  const [content, setContent] = useState("");
+
+  //!------------- Multiple Image Select -------------
   const onSelectFiles = (e) => {
     const imageList = Array.from(e.target.files);
     if (imageList.length > 4) {
@@ -26,14 +35,24 @@ const AddNews = () => {
     }
   };
 
-  console.log(formData);
+  const handleSelectedCategories = (value) => {
+    if (selectedCategories.find((item) => item === value)) {
+      setSelectedCategories(
+        selectedCategories.filter((item) => item !== value)
+      );
+    } else {
+      setSelectedCategories([...selectedCategories, value]);
+    }
+  };
+
+  console.log(selectedCategories);
 
   return (
     <section className="add-news-container">
-      <h2 className="heading-text">Add A Article</h2>
+      <h2 className="heading-text">Add An Article</h2>
       <div>
         <form action="" className="form-container">
-          <div className="flex flex-col lg:flex-row space-x-0 space-y-4 lg:space-x-4 lg:space-y-0">
+          <div className="double-input-container">
             <div className="w-full">
               <input
                 type="text"
@@ -53,7 +72,7 @@ const AddNews = () => {
               />
             </div>
           </div>
-          <div className="flex flex-col lg:flex-row space-x-0 space-y-4 lg:space-x-4 lg:space-y-0">
+          <div className="double-input-container">
             <div className="images-selection-input">
               <label htmlFor="images">
                 <div className="text-center space-y-4">
@@ -73,7 +92,7 @@ const AddNews = () => {
                 {selectedImages.length !== 0 && (
                   <div className="mt-5 grid grid-cols-2 lg:grid-cols-4 gap-6">
                     {selectedImages.map((item, index) => (
-                      <img src={item} key={index} className="block" />
+                      <img src={item} key={index} className="block border" />
                     ))}
                   </div>
                 )}
@@ -100,6 +119,84 @@ const AddNews = () => {
                 </div>
               </div>
             </div>
+          </div>
+          {/*--------- Multiple Select Container ---------*/}
+          <div className="double-input-container">
+            <div className="w-full multiSelect-container">
+              <div
+                className="select-btn"
+                onClick={() => setDropDownDisplay(!dropdownDisplay)}
+              >
+                <div>Select Category</div>
+                <div>
+                  <IoIosArrowDropdown
+                    className={`text-2xl ${
+                      dropdownDisplay ? "rotate-180" : "rotate-0"
+                    } transition-all duration-300 `}
+                  ></IoIosArrowDropdown>
+                </div>
+              </div>
+              <div
+                className={`dropdown ${dropdownDisplay ? "block" : "hidden"}`}
+              >
+                <div
+                  className="dropdown-list"
+                  onClick={() => handleSelectedCategories("item1")}
+                >
+                  <div className="checkbox">
+                    {selectedCategories.find((item) => item === "item1") ? (
+                      <BsCheckCircleFill className="text-xl"></BsCheckCircleFill>
+                    ) : (
+                      <BsCheckCircle className="text-xl"></BsCheckCircle>
+                    )}
+                  </div>
+                  <div className="select-text">Item 1</div>
+                </div>
+                <div
+                  className="dropdown-list"
+                  onClick={() => handleSelectedCategories("item2")}
+                >
+                  <div className="checkbox">
+                    {selectedCategories.find((item) => item === "item2") ? (
+                      <BsCheckCircleFill className="text-xl"></BsCheckCircleFill>
+                    ) : (
+                      <BsCheckCircle className="text-xl"></BsCheckCircle>
+                    )}
+                  </div>
+                  <div className="select-text">Item 2</div>
+                </div>
+                <div
+                  className="dropdown-list"
+                  onClick={() => handleSelectedCategories("item3")}
+                >
+                  <div className="checkbox">
+                    {selectedCategories.find((item) => item === "item3") ? (
+                      <BsCheckCircleFill className="text-xl"></BsCheckCircleFill>
+                    ) : (
+                      <BsCheckCircle className="text-xl"></BsCheckCircle>
+                    )}
+                  </div>
+                  <div className="select-text">Item 3</div>
+                </div>
+              </div>
+            </div>
+            <div className="w-full">
+              <input
+                type="text"
+                placeholder="Sub-Title"
+                name="email"
+                id="email"
+                className="input-field"
+              />
+            </div>
+          </div>
+          <div>
+            <JoditEditor
+              ref={editor}
+              value={content}
+              tabIndex={1} // tabIndex of textarea
+              onChange={(newContent) => setContent(newContent)}
+            />
           </div>
         </form>
       </div>
